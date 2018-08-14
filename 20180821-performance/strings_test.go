79bd89bd85b37f7427a1868231939537afc69d2c
Taco de Wolff <tacodewolff@gmail.com>
2018-08-14 18:14:23 +1200

    Add initial performance presentation

---
+++ 20180821-performance/strings_test.go
@@ -0,0 +1,28 @@
+package talks
+
+import (
+    "testing"
+    "bytes"
+    "strings"
+   )
+
+func BenchmarkStrings(b *testing.B) {
+    s := ""
+    for i := 0; i < b.N; i++ {
+        s += "x"
+    }
+}
+
+func BenchmarkStringsBuilder(b *testing.B) {
+    s := strings.Builder{}
+    for i := 0; i < b.N; i++ {
+        s.WriteString("x")
+    }
+}
+
+func BenchmarkBytesBuffer(b *testing.B) {
+    s := bytes.Buffer{}
+    for i := 0; i < b.N; i++ {
+        s.WriteString("x")
+    }
+}
